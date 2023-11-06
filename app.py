e50311965ebb791287f31cac5e8fb55a955e63bc
Sean Mullins <Smullins998@gmail.com>
2023-11-06 15:15:04 -0500

    Init

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, session
+import random
+
+app = Flask(__name__)
+app.debug = True
+
+#Backend 
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+#Backend 
+@app.route('/music')
+def music():
+    return render_template('music.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/proj_songspy')
+def proj_songspy():
+    return render_template('proj_songspy.html')
+
+@app.route('/proj_reversemomentum')
+def proj_reversemomentum():
+    return render_template('proj_reversemomentum.html')
+
+@app.route('/cv')
+def cv():
+    return render_template('cv.html')
+ 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8090)
